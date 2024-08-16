e163c9ed2d0cf9d3d2768b140764de3dc7b239b2
hinami.higashi <higashi.h27@gmail.com>
2024-08-16 16:29:41 +0900

    二分探索を実装

--- code/main.rb
+++ code/main.rb
@@ -7,7 +7,26 @@ def linear_search(array, target)
   "見つかりませんでした"
 end
 
+def binary_search(array, target)
+  min = 0
+  max = array.length - 1
+  mid = min + (min - max) / 2
+  while min <= max
+    if array[mid] < target
+      min = mid + 1
+      return binary_search(array[min..max], target)
+    elsif array[mid] > target
+      max = mid - 1
+      p array[min..max]
+      return binary_search(array[min..max], target)
+    elsif array[mid] == target
+      return "#{target}が見つかりました"
+    end
+  end
+  "見つかりませんでした"
+end
 
 array = [1, 2, 3, 4, 5]
-target = 3
+target = 8
 puts linear_search(array, target)
+puts binary_search(array, target)
---
+++ makefile
@@ -0,0 +1,8 @@
+up:
+	docker-compose up -d
+down:
+	docker-compose down
+in:
+	docker exec -ti code /bin/bash
+run:
+	docker exec -ti code ruby hello.rb
